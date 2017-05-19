42652b85ac57c6fbec19ea8daee645ee3ba7b8f5
jcalta <jan.calta@here.com>
2017-05-19 23:14:19 +0200

    calculator: Implement polynomial

--- calculator/src/main/scala/calculator/Polynomial.scala
+++ calculator/src/main/scala/calculator/Polynomial.scala
@@ -1,13 +1,16 @@
 package calculator
 
+import scala.math
+
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
-    ???
+    Var((b() * b()) - (4 * a() * c()))
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+        if (delta() < 0) Var(Set())
+        else Var(Set((-b() + scala.math.sqrt(delta()))/(2*a()), (-b() - scala.math.sqrt(delta()))/(2*a())))
   }
 }
